f63a6ddcdd3b5c457f119efbfbb768c374532726
Vikas Sable <vikassable2009@gmail.com>
2016-04-16 16:39:19 -0500

    base code is ready for frontend

---
+++ config/schema.sql
@@ -0,0 +1,13 @@
+create table Debate_topic(
+topic_key int auto_increment primary key,
+topic_name varchar(100),
+topic_date Date,
+page_no int,
+topic_type enum('qwn','mpi','adj')
+
+);
+
+create table Member(
+
+
+)
